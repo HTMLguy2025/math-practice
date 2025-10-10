4bdaeb344f907afb13ace56107c4286948f1fe23
HTMLguy2025 <kathryn.tulk@gmail.com>
2025-10-10 14:14:15 -0500

    added addition and subtraction

--- practice.js
+++ practice.js
@@ -1,6 +1,7 @@
 const LastQuesNum1 = document.querySelector('#previousQuestionNum1');
 const LastQuesNum2 = document.querySelector('#previousQuestionNum2');
 const LastQuesAnswer = document.querySelector('#previousQuestionAnswer');
+const LastQuesOperator = document.querySelector("#previousQuestionOperator");
 const factNumberOne = document.querySelector("#factNum1");
 const factNumberTwo = document.querySelector("#factNum2");
 const Operator = document.querySelector("#operator");
@@ -14,9 +15,13 @@ const button7 = document.querySelector("#button7");
 const button8 = document.querySelector("#button8");
 const button9 = document.querySelector("#button9");
 const button0 = document.querySelector("#button0");
+const multiplyButton = document.querySelector("#multiplyButton");
+const addButton = document.querySelector("#addButton");
+const subtractButton = document.querySelector("#subtractButton");
 const checkButton = document.querySelector("#checkmark");
 const DisplayedSelectedAnswer = document.querySelector("#displayedSelectedAnswer");
 const DeleteButton = document.querySelector("#deleteButton");
+const NegativeButton = document.querySelector("#negativeButton");
 
 var selectedAnswer = ""
 
@@ -26,11 +31,15 @@ var previousQuestionNum2 = "5"
 
 var previousQuestionAnswer = "25"
 
+var previousQuestionOperator = "times"
+
 var currentQuestionNum1 = "4"
 
 var currentQuestionNum2 = "4"
 
 var answer = "16"
+
+var currentQuestionOperator = "times"
 
 
 document.addEventListener('DOMContentLoaded', () => {
@@ -140,36 +149,102 @@ const registerEventListeners = () => {
     });
 
     checkButton.addEventListener('click', () => {
-        if (selectedAnswer = answer) {
-            selectedAnswer = ("|");
-            DisplayedSelectedAnswer.innerHTML = selectedAnswer
+        
+        if (selectedAnswer == answer) {
             previousQuestionNum1 = currentQuestionNum1
             previousQuestionNum2 = currentQuestionNum2
             previousQuestionAnswer = answer
-            currentQuestionNum1 = Math.ceil(Math.random() * 10);
-            currentQuestionNum2 = Math.ceil(Math.random() * 10);
-            answer = currentQuestionNum1 * currentQuestionNum2
+            previousQuestionOperator = currentQuestionOperator
+            if (currentQuestionOperator == "times") {
+                Operator.innerHTML = "x"
+                currentQuestionNum1 = Math.ceil(Math.random() * 10);
+                currentQuestionNum2 = Math.ceil(Math.random() * 10);
+                answer = currentQuestionNum1 * currentQuestionNum2
+            }
+
+            if (currentQuestionOperator == "plus") {
+                Operator.innerHTML = "+"
+                currentQuestionNum1 = Math.ceil(Math.random() * 10);
+                currentQuestionNum2 = Math.ceil(Math.random() * 10);
+                answer = currentQuestionNum1 + currentQuestionNum2
+            }
+
+            if (currentQuestionOperator == "minus") {
+                Operator.innerHTML = "-"
+                currentQuestionNum1 = Math.ceil(Math.random() * 10);
+                currentQuestionNum2 = Math.ceil(Math.random() * 10);
+                answer = currentQuestionNum1 - currentQuestionNum2
+            }
+
+            if (previousQuestionOperator == "times") {
+                LastQuesOperator.innerHTML = "x"
+            }
+
+            if (previousQuestionOperator == "plus") {
+                LastQuesOperator.innerHTML = "+"
+            }
+
+            if (previousQuestionOperator == "minus") {
+                LastQuesOperator.innerHTML = "-"
+            }
+            
             LastQuesNum1.innerHTML = previousQuestionNum1
             LastQuesNum2.innerHTML = previousQuestionNum2
             LastQuesAnswer.innerHTML = previousQuestionAnswer
             factNumberOne.innerHTML = currentQuestionNum1
             factNumberTwo.innerHTML = currentQuestionNum2
+            
+            console.log(previousQuestionNum1 + " " + previousQuestionOperator + " " + previousQuestionNum2 + " = " + previousQuestionAnswer + " - correct");
+            selectedAnswer = ("|");
+            DisplayedSelectedAnswer.innerHTML = selectedAnswer
         }
 
         else {
-            selectedAnswer = ("|");
-            DisplayedSelectedAnswer.innerHTML = selectedAnswer
             previousQuestionNum1 = currentQuestionNum1
             previousQuestionNum2 = currentQuestionNum2
             previousQuestionAnswer = answer
-            currentQuestionNum1 = Math.ceil(Math.random() * 10);
-            currentQuestionNum2 = Math.ceil(Math.random() * 10);
-            answer = currentQuestionNum1 * currentQuestionNum2
+            previousQuestionOperator = currentQuestionOperator
+            if (currentQuestionOperator == "times") {
+                Operator.innerHTML = "x"
+                currentQuestionNum1 = Math.ceil(Math.random() * 10);
+                currentQuestionNum2 = Math.ceil(Math.random() * 10);
+                answer = currentQuestionNum1 * currentQuestionNum2
+            }
+
+            if (currentQuestionOperator == "plus") {
+                Operator.innerHTML = "+"
+                currentQuestionNum1 = Math.ceil(Math.random() * 10);
+                currentQuestionNum2 = Math.ceil(Math.random() * 10);
+                answer = currentQuestionNum1 + currentQuestionNum2
+            }
+
+            if (currentQuestionOperator == "minus") {
+                Operator.innerHTML = "-"
+                currentQuestionNum1 = Math.ceil(Math.random() * 10);
+                currentQuestionNum2 = Math.ceil(Math.random() * 10);
+                answer = currentQuestionNum1 - currentQuestionNum2
+            }
+
+            if (previousQuestionOperator == "times") {
+                LastQuesOperator.innerHTML = "x"
+            }
+
+            if (previousQuestionOperator == "plus") {
+                LastQuesOperator.innerHTML = "+"
+            }
+
+            if (previousQuestionOperator == "minus") {
+                LastQuesOperator.innerHTML = "-"
+            }
+
             LastQuesNum1.innerHTML = previousQuestionNum1
             LastQuesNum2.innerHTML = previousQuestionNum2
             LastQuesAnswer.innerHTML = previousQuestionAnswer
             factNumberOne.innerHTML = currentQuestionNum1
             factNumberTwo.innerHTML = currentQuestionNum2
+            console.log(previousQuestionNum1 + " " + previousQuestionOperator + " " + previousQuestionNum2 + " = " + previousQuestionAnswer + " - incorrect");
+            selectedAnswer = ("|");
+            DisplayedSelectedAnswer.innerHTML = selectedAnswer
         }
     });
 
@@ -177,5 +252,30 @@ const registerEventListeners = () => {
         selectedAnswer = ("|");
         DisplayedSelectedAnswer.innerHTML = selectedAnswer
         console.log("deleted text");
+    });
+
+    multiplyButton.addEventListener('click', () => {
+        currentQuestionOperator = "times"
+        DisplayedSelectedAnswer.innerHTML = "Click checkmark twice"
+    });
+
+    addButton.addEventListener('click', () => {
+        currentQuestionOperator = "plus"
+        DisplayedSelectedAnswer.innerHTML = "Click checkmark twice"
+    });
+
+    subtractButton.addEventListener('click', () => {
+        currentQuestionOperator = "minus"
+        DisplayedSelectedAnswer.innerHTML = "Click checkmark twice"
+    });
+
+    NegativeButton.addEventListener('click', () => {
+        if (selectedAnswer.length < 25) {
+            if (selectedAnswer.includes("|")) {
+                selectedAnswer = ""
+            }
+            selectedAnswer = selectedAnswer + ("-");
+            DisplayedSelectedAnswer.innerHTML = selectedAnswer
+        }
     });
 }
